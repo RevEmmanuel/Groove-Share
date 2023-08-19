3ac4f31597a337a2bc213cf520d6c58aad6e9515
RevEmmanuel <adeolaae1@gmail.com>
2023-08-19 14:23:54 +0100

    first commit

---
+++ src/test/java/org/grooveshare/playlist/PlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grooveshare.playlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
